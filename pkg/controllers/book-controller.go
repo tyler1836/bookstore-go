7de6e0044d725195b2ce5ee65e218515dde09139
unknown <tyler.r.roll@gmail.com>
2024-02-07 23:54:57 -0700

    adding delete and update func

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -3,11 +3,12 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
-	"github.com/tyler1836/bookstore-go/pkg/utils"
+
+	"github.com/gorilla/mux"
 	"github.com/tyler1836/bookstore-go/pkg/models"
+	"github.com/tyler1836/bookstore-go/pkg/utils"
 )
 
 // Book struct from models
@@ -38,4 +39,51 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	Book := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := Book.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	Id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error parsing id")
+	}
+	details := models.DeleteBook(Id)
+	res, _ := json.Marshal(details)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var updateBook = &models.Book{}
+	utils.ParseBody(r, updateBook)
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	Id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error parsing id")
+	}
+	// get the book from the database
+	bookDetails, db := models.GetBookById(Id)
+	// if request has any of these things update it, if empty pass over that field
+	if updateBook.Name != "" {
+		bookDetails.Name = updateBook.Name
+	}
+	if updateBook.Author != "" {
+		bookDetails.Author = updateBook.Author
+	}
+	if updateBook.Publication != "" {
+		bookDetails.Publication = updateBook.Publication
+	}
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,16 +2,16 @@ package utils
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
 // since helper function use blank interface so we can pass in any interface
 // we create for this code we will be passing book interface
-func ParseBody(r *http.Request, x interface()){
-	if body, err := ioutil.ReadAll(r.Body); err == nil(
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := io.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
 			return
 		}
-	)
+	}
 }
